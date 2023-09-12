893ec5664ee495fe291d675862cd9f9a5d4c736f
Joseph Cardona <82360283+jaluscg@users.noreply.github.com>
2023-09-12 16:27:58 +0000

    separar evaluate_formula

--- user_controls/funciones.py
+++ user_controls/funciones.py
@@ -1,24 +1,51 @@
-class Funciones:
-    
-    @staticmethod
-    def get_cell_value(cells, cell_ref):
-        col = ord(cell_ref[0]) - 65  # Convertir A, B, C, ... a 0, 1, 2, ...
-        row = int(cell_ref[1:]) - 1
-        return float(cells[row][col].value)
-    
-    @staticmethod
-    def SUM(cells, *args):
+import re
+
+def get_cell_value(cells, cell_ref):
+    col = ord(cell_ref[0]) - 65  # Convertir A, B, C, ... a 0, 1, 2, ...
+    row = int(cell_ref[1:]) - 1
+    return float(cells[row][col].value)
+
+def evaluate_formula(cells, formula, row, col):
+
+    # Fórmula =SUM(A1, A2, ...)
+    match_sum = re.match(r"=SUM\((?P<args>[A-Z]\d(?:,[A-Z]\d)*)\)", formula)
+    if match_sum:
+        args = match_sum.group('args').split(',')
+        
+        # Uso de get_cell_value
         result = 0
         for cell_ref in args:
-            result += Funciones.get_cell_value(cells, cell_ref)
-        return result
-    
-    @staticmethod
-    def ADD(cells, *args):
+            result += get_cell_value(cells, cell_ref)
+        
+        cells[row][col].value = str(result)
+        return
+
+    # Fórmula =ADD(A1,5,A2,...)
+    match_add = re.match(r"=ADD\((?P<args>[A-Z]\d|[\d.]+(?:,[A-Z]\d|[\d.]+)*)\)", formula)
+    if match_add:
+        args = match_add.group('args').split(',')
+        
+        # Uso de get_cell_value
         result = 0
         for arg in args:
             if re.match(r"^[A-Z]\d$", arg):  # Si el argumento es una referencia de celda
-                result += Funciones.get_cell_value(cells, arg)
+                result += get_cell_value(cells, arg)
             else:  # Si el argumento es un número
                 result += float(arg)
-        return result
+
+        cells[row][col].value = str(result)
+        return
+
+    # Fórmula general que puede contener operaciones y referencias a celdas
+    def replace_cell_reference(match):
+        r = int(match.group(2)) - 1
+        c = ord(match.group(1)) - 65
+        return str(cells[r][c].value)
+    
+    formula_eval = re.sub(r'([A-Z])(\d+)', replace_cell_reference, formula[1:])  # Usamos [1:] para omitir el signo "=" al principio
+
+    try:
+        result = eval(formula_eval)
+        cells[row][col].value = str(result)
+    except Exception as e:
+        cells[row][col].value = "Error"
--- user_controls/table.py
+++ user_controls/table.py
@@ -1,7 +1,7 @@
 import flet as ft
 from flet import *
 import re
-from .funciones import Funciones
+from .funciones import evaluate_formula
 
 class TextFieldTable:
     def __init__(self, rows, cols):
@@ -43,54 +43,17 @@ class TextFieldTable:
         self.highlight_cell(new_selected_cell)
         new_selected_cell.focus()
 
-    
+
     def create_table(self, page):
 
         page.on_keyboard_event = self.on_keyboard_event
 
-            
-        def evaluate_formula(self, formula, row, col):
-    
-            # Fórmula =SUM(A1, A2, ...)
-            match_sum = re.match(r"=SUM\((?P<args>[A-Z]\d(?:,[A-Z]\d)*)\)", formula)
-            if match_sum:
-                args = match_sum.group('args').split(',')
-                result = Funciones.SUM(self.cells, *args)  # Aquí ya no pasamos 'self'
-                self.cells[row][col].value = str(result)
-                return
-
-            # Fórmula =ADD(A1,5,A2,...)
-            match_add = re.match(r"=ADD\((?P<args>[A-Z]\d|[\d.]+(?:,[A-Z]\d|[\d.]+)*)\)", formula)
-            if match_add:
-                args = match_add.group('args').split(',')
-                result = Funciones.ADD(self.cells, *args)  # Aquí ya no pasamos 'self'
-                self.cells[row][col].value = str(result)
-                return
-
-            # Fórmula general que puede contener operaciones y referencias a celdas
-            def replace_cell_reference(match):
-                r = int(match.group(2)) - 1
-                c = ord(match.group(1)) - 65
-                return str(self.cells[r][c].value)
-            
-            formula_eval = re.sub(r'([A-Z])(\d+)', replace_cell_reference, formula[1:])  # Usamos [1:] para omitir el signo "=" al principio
-
-            try:
-                result = eval(formula_eval)
-                self.cells[row][col].value = str(result)
-            except Exception as e:
-                self.cells[row][col].value = "Error"
-
-
-                    
-
-
-
         def on_textfield_change(e):   #se ejecuta cuando se cambia el valor de un textfield
-             if e.control.value.startswith("="):
-                  row, col = e.control.row, e.control.col
-                  evaluate_formula(e.control.value, row, col)
-                  page.update() 
+            pass
+                #if e.control.value.startswith("="):
+                #    row, col = e.control.row, e.control.col
+                #    evaluate_formula(self.cells, e.control.value, row, col)  # Llamamos a la función
+                #    page.update() 
              
         def on_textfield_focus(e):
             self.highlight_cell(e.control)
@@ -100,7 +63,7 @@ class TextFieldTable:
             # Evaluar la fórmula cuando se presiona Enter
             if e.control.value.startswith("="):
                 row, col = e.control.row, e.control.col
-                evaluate_formula(self, e.control.value, row, col)
+                evaluate_formula(self.cells, e.control.value, row, col)
                 page.update()
 
 
@@ -109,7 +72,7 @@ class TextFieldTable:
             # Evaluar la fórmula cuando la celda pierde el foco
             if e.control.value.startswith("="):
                 row, col = e.control.row, e.control.col
-                evaluate_formula(self, e.control.value, row, col)
+                evaluate_formula(self.cells, e.control.value, row, col)
                 page.update()
 
 
