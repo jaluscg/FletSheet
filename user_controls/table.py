e0733b092a1dfa9746ee2fbff9dd40d2c348281d
jaluscg <82360283+jaluscg@users.noreply.github.com>
2023-11-22 21:55:57 +0000

    quitar esas clases

--- user_controls/table.py
+++ user_controls/table.py
@@ -4,22 +4,8 @@ import re
 from .funciones import evaluate_formula
 import openpyxl
 
-from typing import Any, List, Optional, Union
-from flet_core.constrained_control import ConstrainedControl
-from flet_core.control import Control, OptionalNumber
-from flet_core.ref import Ref
-from flet_core.scrollable_control import ScrollableControl
-from flet_core.types import (
-    AnimationValue,
-    OffsetValue,
-    PaddingValue,
-    ResponsiveNumber,
-    RotateValue,
-    ScaleValue,
-    ScrollMode,
-)
 
-class TextFieldTable(ConstrainedControl, ScrollableControl):
+class TextFieldTable():
     """
     Una matriz de celdas que se puede editar y desplazar.
     """
@@ -29,43 +15,6 @@ class TextFieldTable(ConstrainedControl, ScrollableControl):
                 cols,
                 cell_height: OptionalNumber = 30,
                 cell_width: OptionalNumber = 100,
-                controls: Optional[List[Control]] = None,
-                auto_scroll: Optional[bool] = None,
-                on_scroll_interval: OptionalNumber = None,
-                on_scroll: Any = None,
-
-                ref: Optional[Ref] = None,
-                key: Optional[str] = None,
-                width: OptionalNumber = None,
-                height: OptionalNumber = None,
-                left: OptionalNumber = None,
-                top: OptionalNumber = None,
-                right: OptionalNumber = None,
-                bottom: OptionalNumber = None,
-                expand: Union[None, bool, int] = None,
-                col: Optional[ResponsiveNumber] = None,
-                opacity: OptionalNumber = None,
-                rotate: RotateValue = None,
-                scale: ScaleValue = None,
-                offset: OffsetValue = None,
-                aspect_ratio: OptionalNumber = None,
-                animate_opacity: AnimationValue = None,
-                animate_size: AnimationValue = None,
-                animate_position: AnimationValue = None,
-                animate_rotation: AnimationValue = None,
-                animate_scale: AnimationValue = None,
-                animate_offset: AnimationValue = None,
-                on_animation_end=None,
-                visible: Optional[bool] = None,
-                disabled: Optional[bool] = None,
-                data: Any = None,
-                
-                horizontal: Optional[bool] = None,
-                spacing: OptionalNumber = None,
-                item_extent: OptionalNumber = None,
-                first_item_prototype: Optional[bool] = None,
-                divider_thickness: OptionalNumber = None,
-                padding: PaddingValue = None,
                 ):
         
         self.ROWS = rows
@@ -88,70 +37,7 @@ class TextFieldTable(ConstrainedControl, ScrollableControl):
         self.excel_data = self.load_excel_data("./assets/contabilizacion.xlsx")
         self.cell_height = cell_height  
         self.cell_width = cell_width
-
-
-        ConstrainedControl.__init__(
-            self,
-            ref=ref,
-            key=key,
-            width=width,
-            height=height,
-            left=left,
-            top=top,
-            right=right,
-            bottom=bottom,
-            expand=expand,
-            col=col,
-            opacity=opacity,
-            rotate=rotate,
-            scale=scale,
-            offset=offset,
-            aspect_ratio=aspect_ratio,
-            animate_opacity=animate_opacity,
-            animate_size=animate_size,
-            animate_position=animate_position,
-            animate_rotation=animate_rotation,
-            animate_scale=animate_scale,
-            animate_offset=animate_offset,
-            on_animation_end=on_animation_end,
-            visible=visible,
-            disabled=disabled,
-            data=data,
-        )
-
-        ScrollableControl.__init__(
-            self,
-            auto_scroll=auto_scroll,
-            on_scroll_interval=on_scroll_interval,
-            on_scroll=on_scroll,
-        )
-        self.__controls: List[Control] = []
-        self.controls = controls
-        self.horizontal = horizontal
-        self.spacing = spacing
-        self.divider_thickness = divider_thickness
-        self.item_extent = item_extent
-        self.first_item_prototype = first_item_prototype
-        self.padding = padding
-
-    def _get_control_name(self):
-        return "TextFieldTable"
-
-    def _before_build_command(self):
-        super()._before_build_command()
-        self._set_attr_json("padding", self.__padding)
-
-    def _get_children(self):
-        return self.__controls
-
-    def clean(self):
-        super().clean()
-        self.__controls.clear()
-
-    async def clean_async(self):
-        await super().clean_async()
-        self.__controls.clear()
-
+        
 
     def load_excel_data(self, filepath):
         workbook = openpyxl.load_workbook(filepath, data_only=True)
