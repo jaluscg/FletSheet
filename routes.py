38a3ca0f3d9dfac691a5c79fccd92e642f8fdf2c
jaluscg <82360283+jaluscg@users.noreply.github.com>
2023-12-12 22:19:37 +0000

    intento

--- routes.py
+++ routes.py
@@ -4,7 +4,6 @@ from views.index_view import IndexView
 from views.menu import MenuView
 from views.profile import ProfileView
 
-
 app_routes = [
     path(url="/",clear=True,view=IndexView().view), 
     path(url="/menu",clear=True,view= MenuView().view), 
