5db689ed30f44c34100cbedaabbfbbbf4a898a60
jaluscg <82360283+jaluscg@users.noreply.github.com>
2024-01-13 17:35:30 +0000

    cambiar appveyor

--- main.py
+++ main.py
@@ -3,6 +3,24 @@ from flet_route import Routing
 from routes import app_routes
 from middlewares.app_middleware import AppBasedMiddleware
 import sys
+import certifi
+import os
+
+os.environ["REQUESTS_CA_BUNDLE"] = certifi.where()
+os.environ["SSL_CERT_FILE"] = certifi.where()
+
+if os.getenv("FLET_PLATFORM") == "android":
+    import ssl
+
+    def create_default_context(
+        purpose=ssl.Purpose.SERVER_AUTH, *, cafile=None, capath=None, cadata=None
+    ):
+        return ssl.create_default_context(
+            purpose=purpose, cafile=certifi.where(), capath=capath, cadata=cadata
+        )
+
+    ssl._create_default_https_context = create_default_context
+    
 
 
 def main(page: ft.Page):
