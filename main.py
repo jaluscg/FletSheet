df4e1f0d92270b0b8eda93e1788ad0b6c068404c
jaluscg <82360283+jaluscg@users.noreply.github.com>
2024-01-30 17:18:24 +0000

    cambiar a que si hay error se haga el codigo magico para mostrar el log

--- main.py
+++ main.py
@@ -2,24 +2,24 @@ import flet as ft
 from flet_route import Routing
 from routes import app_routes
 from middlewares.app_middleware import AppBasedMiddleware
+import sys
 
-import logging
-
-# Configurar logging para escribir en un archivo
-logging.basicConfig(filename='out.log', level=logging.DEBUG, filemode='w')
 
 
 def main(page: ft.Page):
+    try:
+        Routing(
+            page=page,
+            app_routes=app_routes,
+            middleware=AppBasedMiddleware().call_me
+        )
+        page.go(page.route)
+    except Exception as e:
+        print("Ocurrió un error:", e)
+        sys.exit(100)  # Magic code para mostrar el log
 
-    Routing(
-        page = page,
-        app_routes = app_routes,
-        middleware = AppBasedMiddleware().call_me
-    )
-    page.go(page.route)
 
 
-
-#ft.app(main)
+ft.app(main)
     # En un entorno de desarrollo
-ft.app(target=main, view=ft.AppView.WEB_BROWSER, port=8550)
+#ft.app(target=main, view=ft.AppView.WEB_BROWSER, port=8550)
