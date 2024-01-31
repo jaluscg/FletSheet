4947c10cd1c918d24431161dafecc7850ec67cdc
jaluscg <82360283+jaluscg@users.noreply.github.com>
2024-01-31 15:24:25 +0000

    probar con app facilita a ver si sirve la app

--- main.py
+++ main.py
@@ -1,46 +1,21 @@
 import flet as ft
-from flet_route import Routing
-from routes import app_routes
 import sys
 import logging
-import time
 
-
-# Configurar logging para escribir en un archivo
-#logging.basicConfig(filename='out.log', level=logging.DEBUG, filemode='w')
 
 
 def main(page: ft.Page):
 
-    # Crear y mostrar el indicador de progreso
-    progress_ring = ft.ProgressRing()
+   
 
     page.add(
         ft.SafeArea(
-            content= progress_ring)
+            content= ft.TextButton("Oprimir", on_click=page.go("/menu")))
             )
 
-    try:
-        # Pausar para simular carga (3 segundos)
-        time.sleep(3)
-
-        # Realizar las operaciones mientras se muestra el indicador de progreso
-        Routing(
-            page=page,
-            app_routes=app_routes,
-        )
-        page.go(page.route)
-
-    except Exception as e:
-        print("Ocurrió un error:", e)
-        sys.exit(100)  # Magic code para mostrar el log
-
-    finally:
-        # Ocultar el indicador de progreso al finalizar
-        progress_ring.visible = False
-        page.update()
+   
 
 
-ft.app(target=main)
+ft.app(main)
 
 #ft.app(target=main, view=ft.AppView.WEB_BROWSER, port=8550)
