e19858b4254b15d19bacb8cf26562a84d127d543
Jaluscg <82360283+jaluscg@users.noreply.github.com>
2024-02-07 10:43:28 -0500

    probar usando documentación

--- main.py
+++ main.py
@@ -1,24 +1,44 @@
-import flet as ft
-from flet_route import Routing
-from routes import app_routes
-import sys
-import logging
-import time
+from flet import *
+from views.menu import MenuView
 
 
 
-def main(page: ft.Page):
 
 
-    Routing(
-            page=page,
-            app_routes=app_routes,
+def main(page: Page):
+    page.window_title = "excel con fletsheet"
+    page.horizontal_alignment = 'center'
+    page.padding = 0
+
+    def route_change(route):
+        page.views.clear()
+        page.views.append(
+            ft.View(
+                "/",
+                [
+                    ft.AppBar(title=ft.Text("Fletsheet"), bgcolor=ft.colors.SURFACE_VARIANT),
+                    ft.ElevatedButton("Ir a tablas", on_click=lambda _: page.go("/menu")),
+                ],
+            )
         )
-    
+        if page.route == "/menu":
+            page.views.append(
+                MenuView().View
+            )
+        page.update()
+
+    def view_pop(view):
+        page.views.pop()
+        top_view = page.views[-1]
+        page.go(top_view.route)
+
+    page.on_route_change = route_change
+    page.on_view_pop = view_pop
     page.go(page.route)
 
 
+    
 
-#app(main)
+app(main, assets_dir= 'assets/')
 
-ft.app(main, view=ft.AppView.WEB_BROWSER)
+#app(main, view=ft.AppView.WEB_BROWSER)
