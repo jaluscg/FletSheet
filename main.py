5a8af0e917e0f6bd7619c2e4cc88fde2fe140e2f
Jaluscg <82360283+jaluscg@users.noreply.github.com>
2024-02-07 16:34:06 -0500

    volver a usar la forma que recomienda la documentacion sobre rutas

--- main.py
+++ main.py
@@ -1,24 +1,42 @@
-import flet as ft
-from flet_route import Routing
-from routes import app_routes
-import sys
-import logging
-import time
-
+from flet import ft
+from views.menu import MenuView
 
 
 def main(page: ft.Page):
+    page.window_title = "excel con fletsheet"
+    page.horizontal_alignment = 'center'
+    page.padding = 0
 
-
-    Routing(
-            page=page,
-            app_routes=app_routes,
+    def route_change(route):
+        page.views.clear()
+        page.views.append(
+            ft.View(
+                "/",
+                [
+                    ft.AppBar(title=ft.Text("Fletsheet"), bgcolor=ft.colors.SURFACE_VARIANT),
+                    ft.ElevatedButton("Ir a tablas", on_click=lambda _: page.go("/menu")),
+                ],
+            )
         )
-    
+
+        if page.route == "/menu":
+            page.views.append(
+                MenuView().View
+            )
+        page.update()
+
+    def view_pop(view):
+        page.views.pop()
+        top_view = page.views[-1]
+        page.go(top_view.route)
+
+    page.on_route_change = route_change
+    page.on_view_pop = view_pop
     page.go(page.route)
 
 
 
-app(main, assets_dir='assets/')
 
-#ft.app(main, view=ft.AppView.WEB_BROWSER)
+app(main, assets_dir= 'assets/')
+
+#app(main, view=ft.AppView.WEB_BROWSER)
