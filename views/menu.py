96e65e5ba71f21539a20db2967e2dc6667ec441d
Jaluscg <82360283+jaluscg@users.noreply.github.com>
2024-02-14 20:34:14 +0000

    al final se necesita diferencial de la ruta absoluta del archivo

--- views/menu.py
+++ views/menu.py
@@ -1,6 +1,8 @@
 import flet as ft
 from flet import *
 from user_controls.table import TextFieldTable
+import sys
+import os
 
 
 class MenuView:
@@ -10,7 +12,9 @@ class MenuView:
 
     def view(self,page:ft.page):
         
-        excel_file_path = "assets/contabilizacion.xlsx"
+        
+        excel_file_path = self.get_asset_path("assets/contabilizacion.xlsx")
+
 
         self.table = TextFieldTable(excel_file_path, page.width, page.height)
 
@@ -46,4 +50,15 @@ class MenuView:
         )
         
     
-    
+    def get_asset_path(self, relative_path):
+        """
+        Devuelve la ruta absoluta de un archivo, tanto en el entorno de desarrollo como en el empaquetado.
+        """
+        if getattr(sys, 'frozen', False):
+            # Ruta para el entorno empaquetado
+            base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+        else:
+            # Ruta para el entorno de desarrollo
+            return os.path.join("./", relative_path)
+
+        return os.path.join(base_path, relative_path)
