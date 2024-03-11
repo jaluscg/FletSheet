3e9277ed9d4d68f2b01ff71c26fa531fb5298f23
Jaluscg <82360283+jaluscg@users.noreply.github.com>
2024-03-11 22:13:46 +0000

    ahora funciona bien el &

--- fletsheet_formula/funciones.py
+++ fletsheet_formula/funciones.py
@@ -96,93 +96,104 @@ class Formulas():
 
         self.excel_data = excel_data
 
-        """
+        
         if '&' in formula:
             subformulas = formula.split('&')
             results = []
             for sub in subformulas:
                 sub = sub.strip()
                 if sub.startswith("=TEXT") or sub.startswith("TEXT("):
-                    cell_ref, format_str = self.extract_cell_reference_and_format(sub)
-                    if cell_ref:
-                        cell_value = self.get_cell_value(cells, cell_ref, access_type)
-                        formatted_value = text_formula(cell_value, format_str, access_type,
-                                                        get_cell_value_func=self.get_cell_value, cells=cells)
-                        results.append(formatted_value)
+                    cell_references, formats = self.extract_cell_reference_and_format(sub)
+                    if cell_references:
+                        for cell_ref in cell_references:
+                            date_str = self.get_cell_value(cells, cell_ref, access_type)
+                            result = text_formula(date_str, formats[0] if formats else "Formato desconocido")
+                            results.append(result)
                     else:
-                        print("Subfórmula TEXT con formato incorrecto")
-                        results.append("")  # O manejar el error de manera apropiada
+                        results.append("Error: Referencia de celda o formato no válido.")
+                    
+                 # Manejar subfórmulas SUM
+                elif sub.startswith("=SUM") or sub.startswith("SUM("):
+                    cell_references, _ = self.extract_cell_reference_and_format(sub)
+                    if cell_references:
+                        range_sum = sum_formula(cells, cell_references[0])
+                        results.append(str(range_sum))
+                    else:
+                        results.append("Error: Rango de celda no válido.")
                 else:
-                    # Manejar otras subfórmulas si es necesario
-                    pass
+                    # Procesamiento genérico para otras subfórmulas
+                    # Puedes extender este bloque para manejar otros tipos de subfórmulas específicas.
+                    result = "Fórmula no soportada"
+                    results.append(result)    
+               
 
             
             return ''.join(results)
            
         
-        else:  """
-     
-        if formula.startswith("=TEXT"):
-            cell_references, formats = self.extract_cell_reference_and_format(formula) if access_type == "withexceldata" else ([], None)
-            if access_type != "withexceldata":
-                match = re.match(r'=TEXT\((?P<cell_ref>[A-Z]+\d+); ?"(?P<format_str>dddd|yy|mmmm)"\)', formula)
-                if match:
-                    cell_references = [match.group('cell_ref')]  # Convirtiendo el valor en una lista
-                    formats =  [match.group('format_str')]  
-            if cell_references:
-                for cell_ref in cell_references:
-                    date_str = self.get_cell_value(cells, cell_ref, access_type) if access_type == "withexceldata" else formula
-                    print(f"date_str:{date_str}")
-                    # Asegúrate de pasar una cadena como format_str, usando formats[0] si formats no está vacío
-                    result = text_formula(date_str, formats[0] if formats else "Formato desconocido")
-                    print(f"result:{result}")
-                    return result
-            else:
-                print("Error: Referencia de celda o formato no válido.")
-
-        if formula.startswith("=SUM"):
-            cell_references, formats = self.extract_cell_reference_and_format(formula)
-            if cell_references:
-                range_sum = 0
-                for cell_range in cell_references:
-                    range_sum += sum_formula(cells, cell_range)
-                return range_sum
-            else:
-                print("Error: Rango de celda no válido.")
-                
-
-                    
-            
-                
-        else:
-            if access_type == "withexceldata":
-                # Verificar si la fórmula contiene funciones complejas o rangos, los cuales no queremos evaluar.
-                # Este regex busca funciones comunes de Excel y el uso de ':' para rangos.
-                print(f"formula:{formula}")
-                if re.search(r'(\w+\()|:', formula):
-                    print("La fórmula contiene funciones complejas o rangos que no se evaluarán.")
-                    # Puedes decidir qué hacer en este caso, por ejemplo, retornar un valor por defecto o el mismo texto de la fórmula.
-                    return "Fórmula no soportada"
-                else:
-                    cell_references, _ = self.extract_cell_reference_and_format(formula)
+        else:  
+        
+            if formula.startswith("=TEXT"):
+                cell_references, formats = self.extract_cell_reference_and_format(formula) if access_type == "withexceldata" else ([], None)
+                if access_type != "withexceldata":
+                    match = re.match(r'=TEXT\((?P<cell_ref>[A-Z]+\d+); ?"(?P<format_str>dddd|yy|mmmm)"\)', formula)
+                    if match:
+                        cell_references = [match.group('cell_ref')]  # Convirtiendo el valor en una lista
+                        formats =  [match.group('format_str')]  
+                if cell_references:
                     for cell_ref in cell_references:
-                        cell_value = self.get_cell_value_from_excel_formulas(cell_ref)
-                        cell_value_str = repr(cell_value)
-                        formula = formula.replace(cell_ref, cell_value_str)
+                        date_str = self.get_cell_value(cells, cell_ref, access_type) if access_type == "withexceldata" else formula
+                        print(f"date_str:{date_str}")
+                        # Asegúrate de pasar una cadena como format_str, usando formats[0] si formats no está vacío
+                        result = text_formula(date_str, formats[0] if formats else "Formato desconocido")
+                        print(f"result:{result}")
+                        return result
+                else:
+                    print("Error: Referencia de celda o formato no válido.")
 
-            try:
-                # Procesar la fórmula sencilla utilizando eval para el cálculo final
-                formula_eval = re.sub(r'([A-Z]+)(\d+)', lambda match: str(self.get_cell_value(cells, match.group(0), access_type)), formula.lstrip('='))
-                result = eval(formula_eval)
-                if access_type == "withcell":
-                    return result
-                elif access_type == "withdictionary":
-                   return result
-                elif access_type == "withexceldata":
-                    return result 
+            if formula.startswith("=SUM"):
+                cell_references, formats = self.extract_cell_reference_and_format(formula)
+                if cell_references:
+                    range_sum = 0
+                    for cell_range in cell_references:
+                        range_sum += sum_formula(cells, cell_range)
+                    return range_sum
+                else:
+                    print("Error: Rango de celda no válido.")
+                    
 
-            except Exception as e:
-                print(f"Error evaluando la fórmula: {e}")
-                # Manejar el error de manera apropiada, por ejemplo, asignando un valor de error a la celda.
-                return "Error en fórmula"
-            
+                        
+                
+                    
+            else:
+                if access_type == "withexceldata":
+                    # Verificar si la fórmula contiene funciones complejas o rangos, los cuales no queremos evaluar.
+                    # Este regex busca funciones comunes de Excel y el uso de ':' para rangos.
+                    print(f"formula:{formula}")
+                    if re.search(r'(\w+\()|:', formula):
+                        print("La fórmula contiene funciones complejas o rangos que no se evaluarán.")
+                        # Puedes decidir qué hacer en este caso, por ejemplo, retornar un valor por defecto o el mismo texto de la fórmula.
+                        return "Fórmula no soportada"
+                    else:
+                        cell_references, _ = self.extract_cell_reference_and_format(formula)
+                        for cell_ref in cell_references:
+                            cell_value = self.get_cell_value_from_excel_formulas(cell_ref)
+                            cell_value_str = repr(cell_value)
+                            formula = formula.replace(cell_ref, cell_value_str)
+
+                try:
+                    # Procesar la fórmula sencilla utilizando eval para el cálculo final
+                    formula_eval = re.sub(r'([A-Z]+)(\d+)', lambda match: str(self.get_cell_value(cells, match.group(0), access_type)), formula.lstrip('='))
+                    result = eval(formula_eval)
+                    if access_type == "withcell":
+                        return result
+                    elif access_type == "withdictionary":
+                        return result
+                    elif access_type == "withexceldata":
+                        return result 
+
+                except Exception as e:
+                    print(f"Error evaluando la fórmula: {e}")
+                    # Manejar el error de manera apropiada, por ejemplo, asignando un valor de error a la celda.
+                    return "Error en fórmula"
+                
