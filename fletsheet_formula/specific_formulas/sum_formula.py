1a8d06dc778df1704d770fecf4722c1048d70010
Jaluscg <82360283+jaluscg@users.noreply.github.com>
2024-03-11 22:07:27 +0000

    ahora las sumas salen enteras

--- fletsheet_formula/specific_formulas/sum_formula.py
+++ fletsheet_formula/specific_formulas/sum_formula.py
@@ -1,5 +1,5 @@
 def sum_formula(cells, cell_range):
-    total_sum = 0
+    total_sum = 0.0  # Inicializar total_sum como flotante
     # Descomponer el rango en celda inicial y final
     start_cell, end_cell = cell_range.split(':')
     start_col, start_row = ord(start_cell[0]) - 65, int(start_cell[1:]) - 1
@@ -15,5 +15,9 @@ def sum_formula(cells, cell_range):
             except ValueError:
                 # Maneja la excepción si la conversión falla
                 print(f"No se puede convertir el valor de la celda {chr(col + 65)}{row + 1} a número.")
-    
-    return total_sum
+
+    # Ajuste para evitar el .0 en números enteros
+    if total_sum.is_integer():
+        return int(total_sum)
+    else:
+        return total_sum
